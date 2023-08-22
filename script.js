be548025a0147f1eb207c077c1209af854972654
Akhiil222 <142396414+Akhiil222@users.noreply.github.com>
2023-08-22 03:29:23 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Typing Animation
+var typed = new Typed(".typing",{
+    strings:["HTML EXPERT","Web Developer","Graphic Designer",],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+// Aside
+const nav = document.querySelector(".nav"),
+navList = nav.querySelectorAll
